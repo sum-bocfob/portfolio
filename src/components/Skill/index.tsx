6378f76d8f33f3ba12342700a01f7a8358d7a28b
sum <yuritaso0204@gmail.com>
2025-06-01 23:51:25 +0900

    💫 スキルのゲージをGSAPのScrollTrigerを使って、画面内に入ったら伸ばすアニメーションを追加

--- src/components/Skill/index.tsx
+++ src/components/Skill/index.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useRef } from "react";
 import { type SkillType } from "../../skills";
 import st from "./Skill.module.scss";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 const Skill = ({ name, desc, level, img }: SkillType) => {
+    const gaugeRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const elm = gaugeRef.current;
+        if (!elm) return;
+
+        ScrollTrigger.create({
+            trigger: elm,
+            start: "top 80%",
+            onEnter: () => elm.classList.add(st["meter_gauge__" + level]),
+            once: true,
+        });
+    });
+
     return (
         <li className={st.item}>
             <div className={st.descs}>
                 <div className={st.heading}>
                     <h3>{name}</h3>
-                    {/* <div>{level}</div> */}
                     <div className={st.meter}>
-                        <div className={`${st.meter_gauge} ${st["meter_gauge__" + level]}`}></div>
+                        <div ref={gaugeRef} className={st.meter_gauge}></div>
                     </div>
                 </div>
                 <p>{desc}</p>
