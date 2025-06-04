f52c2f3a4dea76f8f38cfe7d2f335facf5c1bbb2
sum <yuritaso0204@gmail.com>
2025-06-04 17:25:27 +0900

    💄 背景を斜めにした

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -12,6 +12,7 @@ const Section = ({ bgBlack, id, children }: Prop) => {
     return (
         <section id={id} className={`${st.section} ${bgBlack ? st.black : st.white}`}>
             {children}
+            <div className={st.bg_tri}></div>
         </section>
     );
 };
