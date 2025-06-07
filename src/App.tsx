6dadd260367db581a21f9b2752ff0741f9727921
sum <yuritaso0204@gmail.com>
2025-06-07 13:34:18 +0900

    🐛 ページ遷移時にスクロール位置を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import WorkDetail from "./pages/WorkDetail";
+import ScrollToTop from "./components/ScrollToTop";
 // import Works from "./pages/Works";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/#/work_detail/:id" element={<WorkDetail />} />
-        </Routes>
+        <>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/work_detail/:id" element={<WorkDetail />} />
+            </Routes>
+        </>
     );
 }
 
