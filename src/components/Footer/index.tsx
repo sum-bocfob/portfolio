20aeece64ea65353cc8ef38909d792197982873c
sum <yuritaso0204@gmail.com>
2025-06-18 00:59:00 +0900

    ✨ Xリンク追加

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,23 @@
 import Container from "../Container";
 import Heading from "../Heading";
 import st from "./Footer.module.scss";
+import logo_X from "../../assets/logo_x.svg";
 
 const Footer = () => {
     return (
         <footer id="contact" className={st.footer}>
             <Container>
                 <Heading jaText="お問い合わせ" enText="Contact" />
-                <p className={st.text}>下記メールアドレスにてお問い合わせください。</p>
-                <a className={st.address} href="mailto:takuya.hisano1108@gmail.com">
-                    takuya.hisano1108@gmail.com
-                </a>
-            </Container>{" "}
+                <p className={st.text}>下記メールアドレス、SNSにてお問い合わせください。</p>
+                <div className={st.links}>
+                    <a className={st.address} href="mailto:takuya.hisano1108@gmail.com">
+                        takuya.hisano1108@gmail.com
+                    </a>
+                    <a className={st.x_link} href="https://x.com/takuya_hisano" target="_blank">
+                        <img src={logo_X} alt="Xのロゴ" />
+                    </a>
+                </div>
+            </Container>
         </footer>
     );
 };
